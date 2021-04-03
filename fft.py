88356568173070d03cb2fb02eb2325b275940791
dina-shoham <dina.shoham@gmail.com>
2021-04-02 21:12:37 -0600

    initial naive 1d FT code

--- fft.py
+++ fft.py
@@ -1,2 +1,15 @@
 # ECSE 316 - Assignment 2
 # Dina Shoham and Roey Wine
+
+import numpy as np
+
+# naive 1D fourier transform
+
+
+def naive_ft(x):
+    X = []
+    N = len(arr)
+    for k in range(N):
+        for n in range(N):
+            X[k] += x[n] * np.exp(-1j * 2 * np.pi * k * n / N)
+    return X
