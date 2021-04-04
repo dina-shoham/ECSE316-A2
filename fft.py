e636524a388090d336c25450750913889d8e81ef
dina-shoham <dina.shoham@gmail.com>
2021-04-04 16:59:44 -0600

    initial math (1D naive ft, fft, and inverses)

--- fft.py
+++ fft.py
@@ -2,14 +2,89 @@
 # Dina Shoham and Roey Wine
 
 import numpy as np
+import matplotlib.pyplot as plt
 
-# naive 1D fourier transform
 
-
+# naive 1D discrete fourier transform
 def naive_ft(x):
-    X = []
-    N = len(arr)
+    x = np.asarray(a, dtype=complex)
+    N = len(x)
+    X = np.zeros(N, dtype=complex)  # initialize result array as an array of 0s
+
     for k in range(N):
         for n in range(N):
             X[k] += x[n] * np.exp(-1j * 2 * np.pi * k * n / N)
+
     return X
+
+
+# naive 1D inverse discrete fourier transform
+def naive_ift(X):
+    X = np.asarray(a, dtype=complex)
+    N = len(X)
+    x = np.zeros(N, dtype=complex)  # initialize result array as an array of 0s
+
+    for k in range(N):
+        for n in range(N):
+            x[n] = 1/N * X[k] * np.exp(1j * 2 * np.pi * k * n / N)
+
+    return x
+
+
+# 1D cooley-tukey fast fourier transform (divide and conquer algorithm)
+# parameters are x (an array) and n_subproblems, which defines the base case for the algo (default value is 8)
+def fft(x, n_subproblems=8):
+    x = np.asarray(a, dtype=complex)
+    N = len(x)
+    X = np.zeros(N, dtype=complex)
+
+    if N % 2 != 0:
+        raise ValueError("array length must be a power of two")
+
+    # base case
+    elif N <= n_subproblems:
+        return naive_ft(x)
+
+    else:
+        X_even = fft(x[::2])  # all elements at even indeces
+        X_odd = fft(x[1::2])  # all elements at odd indeces
+        coeff = np.exp(-j * 2 * np.pi * np.arange(N // 2) / N)
+
+        X = np.concatenate(X_even + coeff * X_odd, X_even - coeff * X_odd)
+
+        return X
+
+
+# 1D cooley-tukey inverse FFT
+def ifft(X, n_subproblems=8):
+    X = np.asarray(a, dtype=complex)
+    N = len(X)
+    x = np.zeros(N, dtype=complex)
+
+    if N % 2 != 0:
+        raise ValueError("array length must be a power of two")
+
+    # base case
+    elif N <= n_subproblems:
+        return naive_ift(x)
+
+    else:
+        x_even = ifft(X[::2])  # all elements at even indeces
+        x_odd = ifft(X[1::2])  # all elements at odd indeces
+        coeff = 1/N * np.exp(j * 2 * np.pi * np.arange(N // 2) / N)
+
+        x = np.concatenate(x_even + coeff * x_odd, x_even - coeff * x_odd)
+
+        return X
+
+
+def mode_one():
+
+    x = list(range(0, 10))
+    y = list(range(0, 10))
+
+    plt.plot(x, y)
+    plt.show()
+
+
+mode_one()
